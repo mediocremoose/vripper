4cd1423fd99d3dcc3e548f65010f094cd3b7f464
Mialur <Mialur@users.noreply.github.com>
2017-06-21 19:25:29 +0300

    Added comments and " | "
    Added comments, changed white space trimming and added " | "

--- src/tools/cleanFilename.js
+++ src/tools/cleanFilename.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const illegalRe = /[\/\?<>\\:\*\"]/g
-const controlRe = /[\x00-\x1f\x80-\x9f]/g
-const reservedRe = /(^|\/)\.+/
-const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i
-const trailingPeriods = /\.(?=\.*$)/g
+const illegalRe = /[\/\?<:>\\\|\*\"]/g          // characters / ? < : > | * "
+const controlRe = /[\x00-\x1f\x80-\x9f]/g       // control chars with code 0..31 and 128..159
+const reservedRe = /(^|\/)\.+/                  // remove starting . dots "/.." or ".."
+const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i   // remove windows reserved names 
+const trailingPeriods = /\.+$)/g                 // remove trailing periods
 
 /**
  * @param {string} original
@@ -16,7 +16,8 @@ const cleanFilename = (original) => original
   .replace(reservedRe, '')
   .replace(windowsReservedRe, '')
   .replace(trailingPeriods, '')
-  .substr(0, 99)
-  .trim()
+  .replace(/\s+/g,' ')                            // shorten all whitespace only for one char long
+  .trim()                                         // trim white space from both ends
+  .substr(0, 99)                                  // limit max file length to 100 chars
 
 module.exports = cleanFilename
