42089edd59a4688934c088b351c6086c0dc22c12
Mialur <Mialur@users.noreply.github.com>
2017-06-21 20:24:31 +0300

    typo

--- src/tools/cleanFilename.js
+++ src/tools/cleanFilename.js
@@ -4,7 +4,7 @@ const illegalRe = /[\/\?<:>\\\|\*\"]/g          // characters / ? < : > | * "
 const controlRe = /[\x00-\x1f\x80-\x9f]/g       // control chars with code 0..31 and 128..159
 const reservedRe = /(^|\/)\.+/                  // remove starting . dots "/.." or ".."
 const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i   // remove windows reserved names 
-const trailingPeriods = /\.+$)/g                 // remove trailing periods
+const trailingPeriods = /\.+$/g                 // remove trailing periods
 
 /**
  * @param {string} original
